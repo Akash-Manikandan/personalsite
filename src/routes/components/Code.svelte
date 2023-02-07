3dfd1ce550b1d9c59821a37ec9a0c6159544150e
Akash <67492990+Akash-Manikandan@users.noreply.github.com>
2023-02-07 21:18:28 +0530

    Hellloo

--- src/routes/components/Code.svelte
+++ src/routes/components/Code.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import Prism from 'prismjs';
 	import CryptoJS from 'crypto-js';
-	let code = `console.log('Hello world')`;
+
 	export let data: string;
 </script>
 
-<div class="code text-left">
+<code class="code text-left">
 	{@html Prism.highlight(
 		CryptoJS.AES.decrypt(data, 'CodeforPresonalSite').toString(CryptoJS.enc.Utf8),
 		Prism.languages.js,
 		'js'
 	)}
-</div>
+</code>
 
-<style>
+<style style="scss">
 	.code {
 		white-space: pre-wrap;
 	}
